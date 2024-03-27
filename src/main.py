c6d0baa77688556a84d66a112b1ff7b36698e4d2
Denise Müller <134210655+dzzling@users.noreply.github.com>
2024-03-27 14:53:14 +0100

    Get total and aggregated expenses

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 # %%
+# Imports
+
 import csv
 import json
 import os
@@ -7,35 +9,34 @@ import polars as pl
 # %%
 # Funktionstabelle in JSON bereitstellen
 
-with open("../input_data/funktionen_formatted.csv", newline="") as csvfile:
+""" with open("../input_data/funktionen_formatted.csv", newline="") as csvfile:
     reader = csv.DictReader(csvfile)
     data = [row for row in reader]
 
 
 with open("../output_data/funktionen_formatted.json", "w") as jsonfile:
-    json.dump(data, jsonfile, ensure_ascii=False)
+    json.dump(data, jsonfile, ensure_ascii=False) """
 
 # %%
-# Funltionsdataframe erstellen
+# Funktionsdataframe erstellen
 
 with open("../output_data/funktionen_formatted.json", "r") as jsonfile:
     json_data = json.load(jsonfile)
     hauptfunktionen = [x for x in json_data if x["Typ"] == "Hauptfunktion"]
 
-print(hauptfunktionen)
 
 # %%
 # Bundeshaushaltsdaten
 
 schema = {
-    "einzelplan": pl.Int64,
+    "einzelplan": pl.String,
     "einzelplan_text": pl.String,
     "einnahmen_ausgaben": pl.String,
     "einnahmen_ausgaben_text": pl.String,
     "kapitel": pl.String,
     "kapitel_text": pl.String,
     "titel": pl.String,
-    "funktion": pl.Int64,
+    "funktion": pl.String,
     "titel_text": pl.String,
     "flex": pl.String,
     "seite": pl.Int64,
@@ -45,7 +46,7 @@ schema = {
 }
 
 temp = pl.read_csv(
-    "../input_data/haushalt_daten/hh_2023.csv",
+    "../input_data/haushalt_daten/hh_2022.csv",
     separator=";",
     schema=schema,
 )
@@ -56,14 +57,11 @@ converted_data = pl.Series([int(str(number)[0]) for number in unconverted_data])
 hh23 = temp.with_columns(converted_data.alias("funktion"))
 
 # %%
-
-
-# %%
 # Addiere Einträge nach Hauptfunktion
 ctx = pl.SQLContext(register_globals=True, eager_execution=True)
-hh23_small = ctx.execute(
-    "SELECT SUM(soll), funktion, titel_text, einnahmen_ausgaben from hh23 WHERE einnahmen_ausgaben='a' GROUP BY funktion ORDER BY soll DESC LIMIT 10"
+hh23_aggregated = ctx.execute(
+    "SELECT SUM(soll), funktion, titel_text, einnahmen_ausgaben from hh23 WHERE einnahmen_ausgaben='A' OR einnahmen_ausgaben='a' GROUP BY funktion ORDER BY soll DESC"
 )
-print(hh23_small)
-
-# %%
+total_expenses = ctx.execute(
+    "SELECT SUM(soll) from hh23 WHERE einnahmen_ausgaben='A' OR einnahmen_ausgaben='a'"
+)
